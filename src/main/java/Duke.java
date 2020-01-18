76e061b77aba20d67dc7740181eedfa7d0cadd2a
yantingsanity <lytemail18@gmail.com>
2020-01-18 16:43:30 +0800

    duke level one edited

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+        String logo = "  __  .__             /\\            .___     __           \n" +
+                "_/  |_|__| ____    ___)/ ______   __| _/_ __|  | __ ____  \n" +
+                "\\   __\\  |/    \\  / ___\\/  ___/  / __ |  |  \\  |/ // __ \\ \n" +
+                " |  | |  |   |  \\/ /_/  >___ \\  / /_/ |  |  /    <\\  ___/ \n" +
+                " |__| |__|___|  /\\___  /____  > \\____ |____/|__|_ \\\\___  >\n" +
+                "              \\//_____/     \\/       \\/          \\/    \\/ ";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you, today?");
+        System.out.println("____________________________________________________________________");
+
+        Scanner input = new Scanner(System.in);
+        String userInput = input.next();
+
+        while (!userInput.equals("bye")){
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________________");
+            userInput = input.next();
+        }
+        System.out.println("BYE BYE SEE YOU BACK SOON!");
+        input.close();
     }
 }
