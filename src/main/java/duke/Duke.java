31511f007b889812352c42d74f951f53e41fde07
yantingsanity <lytemail18@gmail.com>
2020-02-16 12:03:08 +0800

    Level-6: Deletion of task

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,14 +1,18 @@
 package duke;
 
 import duke.exceptions.InvalidInputException;
+import duke.task.Task;
 import duke.task.commands.Deadline;
 import duke.task.commands.Event;
-import duke.task.Task;
 import duke.task.commands.ToDo;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+
+    private static ArrayList <Task> totalTasks = new ArrayList<>();
 
     public static void getWelcomeMessage(){
         String logo = "  __  .__             /\\            .___     __           \n" +
@@ -22,40 +26,7 @@ public class Duke {
         System.out.println("____________________________________________________________________");
     }
 
-    public static Task createNewTask(String userInput) throws InvalidInputException {
-        String [] task = userInput.split("/");
-        String [] taskDescription = task[0].split(" ", 2);
-        String [] taskDate = new String[0];
-        if (task.length > 1) {
-            taskDate = task[1].split(" ", 2);
-        }
-        Task newTask = null;
-        try {
-            switch (taskDescription[0].trim()){
-            case "todo":
-                newTask = new ToDo(taskDescription[1]);
-                break;
-            case "event":
-                newTask = new Event(taskDescription[1], taskDate[1]);
-                break;
-            case "deadline":
-                newTask = new Deadline(taskDescription[1], taskDate[1]);
-                break;
-            default:
-                throw new InvalidInputException();
-            }
-            System.out.println("Got it. I've added this task:");
-            System.out.println(newTask);
-            return newTask;
-
-        } catch (IndexOutOfBoundsException e){
-            getHelpMessage();
-            return null;
-        }
-    }
-
     public static void getHelpMessage() {
-        System.out.println("There is a missing parameter in your input!");
         System.out.println("Some help for you!! :)");
         System.out.println("3 different ways to add tasks into the list:");
         System.out.println("+ event <event_name> /at <event date> <event time>");
@@ -63,49 +34,105 @@ public class Duke {
         System.out.println("+ deadline <deadline_task> /by <deadline date> <deadline time>");
     }
 
-    public static void printAllTasks(Task [] totalTasks, int index){
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < index; i += 1){
-            System.out.println((i + 1) + ". " + totalTasks[i]);
+    public static void printAllTasks(){
+        if (totalTasks.size() > 0){
+            System.out.println("Here are the tasks in your list:");
+            int index = 0;
+            for (Task task : totalTasks){
+                System.out.println((++index) + ". " + task);
+            }
+        } else {
+            System.out.println("There are currently no tasks in your list!");
         }
     }
 
-    public static void updateTaskStatus(Task taskToCheck){
-        taskToCheck.setTaskAsDone();
-        System.out.println("Nice! I have marked this task as done:");
-        System.out.println(taskToCheck);
+    public static void updateTaskStatus(String userInput){
+        String [] inputStrings = userInput.split(" ");
+        String updateType = inputStrings[0];
+        if (inputStrings.length > 1) {
+            int index = Integer.parseInt(inputStrings[1].trim()) - 1;
+            Task task = null;
+            try {
+                task = totalTasks.get(index);
+                if (updateType.equals("done")) {
+                    task.setTaskAsDone();
+                    System.out.println("Nice! I have marked this task as done:");
+                    System.out.println(task);
+                } else if (updateType.equals("delete")) {
+                    System.out.println("Noted. I've removed this task:");
+                    task.setTaskAsDone();
+                    System.out.println(task);
+                    totalTasks.remove(index);
+                    System.out.println("Now you have " + totalTasks.size() + " in the list.");
+                } else {
+                    getHelpMessage();
+                }
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Please provide an appropriate index to update/delete!");
+            }
+        } else {
+            System.out.println("Please provide an index to update/delete!");
+        }
+    }
+
+    public static void addNewTask(String userInput) throws InvalidInputException{
+        String [] taskStrings = userInput.split("/");
+        String [] taskDescriptions = taskStrings[0].split(" ", 2);
+        String [] taskDate = new String[0];
+        if (taskStrings.length > 1){
+            taskDate = taskStrings[1].split(" ", 2);
+        }
+        try {
+            Task newTask;
+            switch (taskDescriptions[0].trim()){
+            case "todo":
+                newTask = new ToDo(taskDescriptions[1]);
+                break;
+            case "event":
+                newTask = new Event(taskDescriptions[1], taskDate[1]);
+                break;
+            case "deadline":
+                newTask = new Deadline(taskDescriptions[1], taskDate[1]);
+                break;
+            default:
+                throw new InvalidInputException();
+            }
+            System.out.println("Got it. I've added this task:");
+            System.out.println(newTask);
+            //Add new task
+            totalTasks.add(newTask);
+            System.out.println("Now you have " + totalTasks.size() + " tasks in the list.");
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("There is a missing parameter in your input!");
+            getHelpMessage();
+        }
     }
 
     public static void main(String[] args){
-        Task [] totalTasks = new Task[100];
-        int index = 0;
 
         getWelcomeMessage();
 
         Scanner input = new Scanner(System.in);
         String userInput = input.nextLine();
-        try {
-            while (!userInput.equals("bye")){
-                if (userInput.equals("list")){
-                    printAllTasks(totalTasks, index);
-                } else if (userInput.contains("done")){
-                    String [] splitInput = userInput.split(" ");
-                    updateTaskStatus(totalTasks[Integer.parseInt(splitInput[1].trim()) - 1]);
-                } else {
-                    totalTasks[index] = createNewTask(userInput);
-                    if (totalTasks[index] != null) {
-                        index += 1;
-                        System.out.println("Now you have " + index + " tasks in the list.");
-                    }
+
+        while (!userInput.equals("bye")) {
+            if (userInput.equals("list")){
+                printAllTasks();
+            } else if (userInput.contains("done") || userInput.contains("delete")){
+                updateTaskStatus(userInput);
+            } else {
+                try {
+                    addNewTask(userInput);
+                } catch (InvalidInputException e){
+                    System.out.println("OOPS ONO, I'm so sorry, but I don't know what this (" + userInput + ") means");
+                    System.out.println("Please try again!");
                 }
-                System.out.println("____________________________________________________________________");
-                userInput = input.nextLine();
             }
-        } catch (InvalidInputException e){
-            System.out.println("OOPS ONO, I'm so sorry, but I don't know what this (" + userInput + ") means");
+            System.out.println("____________________________________________________________________");
+            userInput = input.nextLine();
         }
 
-        System.out.println("BYE BYE SEE YOU BACK SOON!");
+        System.out.println("BYE BYE SEE YOU SOON!");
         input.close();
     }
 }
